2d9ba4a54a83d62ea7d788148d5038967c5fceab
AIRI <rhee@airi.kr>
2018-06-19 10:42:53 -0400

    .

--- cifar10_estimator/do-preprocess.sh
+++ cifar10_estimator/do-preprocess.sh
@@ -1,6 +1,6 @@
 :
 set -x
-mkdir -p data/1 data/2 data3
+mkdir -p ../data/cifar10-[123]
 python generate_cifar10_tfrecords.py --data-dir=${PWD}/../data/cifar10-1
 python generate_cifar10_tfrecords.py --data-dir=${PWD}/../data/cifar10-2 --radial_fft_noise=11.0
 python generate_cifar10_tfrecords.py --data-dir=${PWD}/../data/cifar10-3 --random_fft_noise=0.1
